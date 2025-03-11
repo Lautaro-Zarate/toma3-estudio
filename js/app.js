18b9a5db60b0611310fb080783c3a6881dc65328
Lautaro Zarate <zlautaro43@gmail.com>
2025-03-11 12:38:44 -0300

    Corrección de texto en boton de wpp

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 function enviarWpp(){
     const numeroWhatsapp = 5491162092628;
-    const mensaje = "Hola, me gustaría consultar la disponibilidad para una sesión fotográfica. Aquí están los detalles de mi evento:"
-    "📅 Fecha del evento: ..."
-    "📍 Lugar: ..."
-    "📖 Tipo de sesión/evento: [Ej.: Boda, cumpleaños, sesión personal, etc.]";
+    const mensaje = `Hola, me gustaría consultar la disponibilidad para una sesión fotográfica. Aquí están los detalles de mi evento:
+    📅 Fecha del evento: [Ingresar fecha]
+    📍 Lugar: [Ingresar ubicación]
+    📖 Tipo de sesión/evento: [Ej.: Boda, cumpleaños, sesión personal, etc.]
+    💬 Comentarios adicionales: [Opcional]`;
     const url = `https://wa.me/${numeroWhatsapp}?text=${encodeURIComponent(mensaje)}`;
     window.open(url, "_blank")
 }
+
+
