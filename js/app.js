ddc44e504aa057df0190e5745b4fe82688f99b39
Lautaro Zarate <zlautaro43@gmail.com>
2025-03-11 12:30:39 -0300

    Cambio de mensaje en el boton de whatsapp

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 function enviarWpp(){
     const numeroWhatsapp = 5491162092628;
-    const mensaje = "Hola, me interesaría saber los precios";
+    const mensaje = "Hola, me gustaría consultar la disponibilidad para una sesión fotográfica. Aquí están los detalles de mi evento:"
+    "📅 Fecha del evento: ..."
+    "📍 Lugar: ..."
+    "📖 Tipo de sesión/evento: [Ej.: Boda, cumpleaños, sesión personal, etc.]";
     const url = `https://wa.me/${numeroWhatsapp}?text=${encodeURIComponent(mensaje)}`;
     window.open(url, "_blank")
 }
